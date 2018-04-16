8336cd3205055c593e6bb0e65ac6769c6ac6ddd1
JohnRoss2407 <38443489+JohnRoss2407@users.noreply.github.com>
2018-04-16 17:20:52 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(){
+    std::cout << "This is our final project\n"
+            "Taylor Hartwell, Zachary Huston\n"
+            "Stefan Papakastopolous, John Ross\n";
+    return 0;
+}
